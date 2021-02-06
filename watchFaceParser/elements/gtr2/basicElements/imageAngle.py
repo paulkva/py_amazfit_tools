14e82aeb2074c11e12289af7758a64e5cca91063
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-02-06 07:26:33 +0100

    comments

--- watchFaceParser/elements/gtr2/basicElements/imageAngle.py
+++ watchFaceParser/elements/gtr2/basicElements/imageAngle.py
@@ -2,9 +2,9 @@
 
 class ImageAngle:
     definitions = {
-        1: { 'Name': 'X', 'Type': 'long'},
-        2: { 'Name': 'Y', 'Type': 'long'}, 
-        3: { 'Name': 'StartAngle', 'Type': 'float'}, 
-        4: { 'Name': 'EndAngle', 'Type': 'float'}, 
-        5: { 'Name': 'Radius', 'Type': 'float'}, 
+        1: { 'Name': 'X', 'Type': 'long'}, # circle center
+        2: { 'Name': 'Y', 'Type': 'long'}, # circle center
+        3: { 'Name': 'StartAngle', 'Type': 'float'}, # 0 center top, 0 - (-180) - left, 0 - 180 - right
+        4: { 'Name': 'EndAngle', 'Type': 'float'}, # 0 center top, 0 - (-180) - left, 0 - 180 - right
+        5: { 'Name': 'Radius', 'Type': 'float'}, # radius from center to middle of indicator line
     }
