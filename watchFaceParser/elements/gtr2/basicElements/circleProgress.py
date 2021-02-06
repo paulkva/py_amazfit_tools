59325c214ba2a86fdecd6aad7c81f4f0d0f73dc8
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-02-06 07:25:49 +0100

    fix parameter name

--- watchFaceParser/elements/gtr2/basicElements/circleProgress.py
+++ watchFaceParser/elements/gtr2/basicElements/circleProgress.py
@@ -5,10 +5,10 @@ class CircleProgress:
     definitions = { 
         1: { 'Name': 'AngleSettings', 'Type': ImageAngle},
         2: { 'Name': 'Unknown2', 'Type': 'long'},
-        3: { 'Name': 'Radius', 'Type': 'long'},
+        3: { 'Name': 'ForegroundImageIndex', 'Type': 'long'},
         4: { 'Name': 'Color', 'Type': Color},   
         5: { 'Name': 'Width', 'Type': 'long'},   
         6: { 'Name': 'Flatness', 'Type': 'long'}, #0 - flat, 90, triangle, 180 - arc 
         7: { 'Name': 'Unknown7', 'Type': 'long'},
-        8: { 'Name': 'ImageIndex', 'Type': 'long'},
+        8: { 'Name': 'BackgroundImageIndex', 'Type': 'long'},
     }
