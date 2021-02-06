530cabfdfd37d509a6f8ba4716c598c0540a570d
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-02-06 10:31:17 +0100

    paddingzero - boolean

--- watchFaceParser/elements/gtr2/basicElements/text.py
+++ watchFaceParser/elements/gtr2/basicElements/text.py
@@ -8,6 +8,6 @@ class Text:
         2: { 'Name': 'SystemFont', 'Type': SystemFont},
         3: { 'Name': 'Alignment', 'Type': TextAlignmentGTR2},
         4: { 'Name': 'Spacing', 'Type': 'long'}, 
-        5: { 'Name': 'PaddingZero', 'Type': 'long'},
+        5: { 'Name': 'PaddingZero', 'Type': 'bool'},
         6: { 'Name': 'Unknown6', 'Type': 'long'}, 
     }
