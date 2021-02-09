971408c7c67dc760a783fcb2f1a2a28bb9655a81
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-02-09 17:09:59 +0100

    ImageIndex2 (id 3) and ImageIndex3 (id 4)

--- watchFaceParser/elements/gtr2/basicElements/imageCoord.py
+++ watchFaceParser/elements/gtr2/basicElements/imageCoord.py
@@ -4,5 +4,6 @@ class ImageCoord:
     definitions = {
         1: { 'Name': 'Coordinates', 'Type': Coordinates}, 
         2: { 'Name': 'ImageIndex', 'Type': 'long'},
-        3: { 'Name': 'Unknown3', 'Type': 'long'},
+        3: { 'Name': 'ImageIndex2', 'Type': 'long'},
+        4: { 'Name': 'ImageIndex3', 'Type': 'long'},
     }
