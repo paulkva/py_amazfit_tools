3ef0d1e2df064c5e3fcd6b64bf9e4aa33b32bfa5
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-03-04 07:35:08 +0100

    shortcut

--- watchFaceParser/elements/gtr2/activity.py
+++ watchFaceParser/elements/gtr2/activity.py
@@ -3,6 +3,7 @@ from watchFaceParser.elements.gtr2.basicElements.imageCoord import ImageCoord
 from watchFaceParser.elements.gtr2.basicElements.imageProgress import ImageProgress 
 from watchFaceParser.elements.gtr2.basicElements.progressBar import ProgressBar  
 from watchFaceParser.models.gtr2.activityType import ActivityType
+from watchFaceParser.elements.gtr2.shortcut import Shortcut
 from watchFaceParser.elements.gtr2.analogDialFaceElements.clockHand import ClockHand 
 
 class Activity:
@@ -12,5 +13,6 @@ class Activity:
         3: { 'Name': 'ProgressBar', 'Type': ProgressBar }, 
         4: { 'Name': 'ImageProgress', 'Type': ImageProgress },
         5: { 'Name': 'Digits', 'Type': [DigitalCommonDigit] },   
-        7: { 'Name': 'Icon', 'Type': ImageCoord },   
+        6: { 'Name': 'Shortcut', 'Type': Shortcut },
+        7: { 'Name': 'Icon', 'Type': ImageCoord },
     }
---
+++ watchFaceParser/elements/gtr2/shortcut.py
@@ -0,0 +1,7 @@
+﻿from watchFaceParser.elements.gtr2.boxElement import BoxElement
+
+class Shortcut:
+    definitions = {
+        1: { 'Name': 'BoxElement', 'Type': BoxElement},
+        2: { 'Name': 'ImageIndex', 'Type': 'long'},
+    }
