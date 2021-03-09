bc08f333c0d04a1d2e7f6c504b191535735392fa
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-03-09 08:58:41 +0100

    progressdialface

--- watchFaceParser/elements/gtr2/progressDialFace.py
+++ watchFaceParser/elements/gtr2/progressDialFace.py
@@ -2,6 +2,8 @@ from watchFaceParser.elements.gtr2.basicElements.progressBar import ProgressBar
 
 class ProgressgDialFace:
     definitions = { 
-        1: { 'Name': 'ProgressBar', 'Type': ProgressBar},
+        1: { 'Name': 'Hours', 'Type': ProgressBar},
+        2: { 'Name': 'Minutes', 'Type': ProgressBar},
+        3: { 'Name': 'Seconds', 'Type': ProgressBar},
     }
 
