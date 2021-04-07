c247b982c5e90254264325ec4a77594bea666e92
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-04-07 13:50:20 +0200

    fix old unknown shortcut - energysaving

--- watchFaceParser/elements/shortcuts.py
+++ watchFaceParser/elements/shortcuts.py
@@ -5,6 +5,6 @@ class Shortcuts:
         1: { 'Name': 'State', 'Type': Shortcut},
         2: { 'Name': 'Pulse', 'Type': Shortcut},
         3: { 'Name': 'Weather', 'Type': Shortcut},
-        4: { 'Name': 'Unknown4', 'Type': Shortcut},
+        4: { 'Name': 'EnergySaving', 'Type': Shortcut},
     }
 
