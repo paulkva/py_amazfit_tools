b2b260905066d815ad29dbeb4f529139f12459e9
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-05-17 15:42:28 +0200

    preview: fix draw order time elements

--- watchFaceParser/models/gtr2/elements/screenNormalElement.py
+++ watchFaceParser/models/gtr2/elements/screenNormalElement.py
@@ -22,6 +22,15 @@ class ScreenNormalElement(ContainerElement):
     def getProgressDialFace(self):
         return self._progressdialface
 
+    def draw3(self, drawer, images, state):
+        print(state.getTime())
+        if self.getDigitalDialFace():
+            self.getDigitalDialFace().draw3(drawer, images, state)
+        if self.getProgressDialFace():
+            self.getProgressDialFace().draw3(drawer, images, state)
+        if self.getAnalogDialFace():
+            self.getAnalogDialFace().draw3(drawer, images, state)
+
 
     def createChildForParameter(self, parameter):
         parameterId = parameter.getId()
