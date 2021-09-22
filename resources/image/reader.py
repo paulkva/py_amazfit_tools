34c95c3729f5c156b52045bb630912036c252c74
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-22 15:35:31 +0200

    read and write resources

--- resources/image/reader.py
+++ resources/image/reader.py
@@ -9,6 +9,8 @@ class Reader():
     def __init__(self, stream):
         self._reader = stream
         self._bip = True
+        self._bme = False
+        self._bmc = False
 
 
     def read(self):
@@ -21,10 +23,33 @@ class Reader():
             logging.warn("The image is 32bit.")
             self._bip = False
 
-        if self._bip:
-            assert(False) # not implemented
+        if signature[0] == ord('B') and signature[1] == ord('M') and signature[2] == ord('e'):
+            self._bme = True
+
+        if signature[0] == ord('B') and signature[1] == ord('M') and signature[2] == 0x1c:
+            self._bmc = True
+        if signature[0] == ord('B') and signature[1] == ord('M') and signature[2] == 0x09:
+            self._bmc = True
+
+        if self._bme:
+            logging.info("The image is bme.")
+            self.readHeader16E()
+            return self.readImage16E()
+        elif self._bmc:
+            logging.info("The image is bmc.")
+            self.readHeader16()
+            if self._bitsPerPixel == 32:
+                return self.readImage()
+            elif self._bitsPerPixel == 24:
+                return self.readImage24()
+            else:
+                return self.readImage16()
         else:
-            self.readHeader()
+            if self._bip:
+                self.readHeader16()
+            else:
+                self.readHeader()
+
             if self._bitsPerPixel == 32:
                 return self.readImage()
             else:
@@ -54,35 +79,122 @@ class Reader():
                 color = resources.image.color.Color.fromArgb(alpha, r, g, b)
                 image.putpixel((x,y), color)
         return image
-    
+
+    def readImage16E(self):
+        image = Image.new('RGBA', (self._width, self._height))
+
+        dataIndex = 0
+        while dataIndex < self._datasize:
+            _y = int.from_bytes(self._reader.read(2), byteorder='little')
+            _start = int.from_bytes(self._reader.read(2), byteorder='little')
+            _width = int.from_bytes(self._reader.read(2), byteorder='little')
+            dataIndex += 6
+            # logging.info(f"y: {_y}, start: {_start}, _width: {_width}")
+
+            rowBytes = self._reader.read(_width*2)
+
+            for x in range(_width):
+                firstByte = rowBytes[x * self._step]
+                secondByte = rowBytes[x * self._step + 1]
+                dataIndex += 2
+                r = ((secondByte >> 3) & 0x1f) << 3
+                g = (((firstByte >> 5) & 0x7) | ((secondByte & 0x07) << 3)) << 2
+                b = (firstByte & 0x1f) << 3
+                alpha = 255
+                color = resources.image.color.Color.fromArgb(alpha, r, g, b)
+                image.putpixel(( _start + x, _y), color)
+
+        return image
+
     def readImage16(self):
         image = Image.new('RGBA', (self._width, self._height))
+
         for y in range(self._height):
             rowBytes = self._reader.read(self._rowLengthInBytes)
             for x in range(self._width):
-                firstByte = rowBytes[x * self._step];
-                secondByte = rowBytes[x * self._step + 1];
-                r = ((secondByte >> 3) & 0x1f) << 3;
-                g = (((firstByte >> 5) & 0x7) | ((secondByte & 0x07) << 3)) << 2;
-                b = (firstByte & 0x1f) << 3;
+                firstByte = rowBytes[x * self._step]
+                secondByte = rowBytes[x * self._step + 1]
+                r = ((secondByte >> 3) & 0x1f) << 3
+                g = (((firstByte >> 5) & 0x7) | ((secondByte & 0x07) << 3)) << 2
+                b = (firstByte & 0x1f) << 3
                 alpha = 255
                 color = resources.image.color.Color.fromArgb(alpha, r, g, b)
-                image.putpixel((x,y), color)
+                image.putpixel((x, y), color)
         return image
 
+    def readImage24(self):
+        image = Image.new('RGBA', (self._width, self._height))
+        a = 0
+        if self._step != 3:
+            a = 255
+        for y in range(self._height):
+            rowBytes = self._reader.read(self._rowLengthInBytes)
+            for x in range(self._width):
+                firstByte = rowBytes[x * self._step]
+                secondByte = rowBytes[x * self._step + 1]
+                thirddByte = rowBytes[x * self._step + 2]
 
-    def readHeader(self):
-        logging.info("Reading image header(non-bip)...")
-        self._width = int.from_bytes(self._reader.read(4), byteorder='little')
-        self._height = int.from_bytes(self._reader.read(4), byteorder='little')
-        self._bitsPerPixel = int.from_bytes(self._reader.read(4), byteorder='little')
-        self._unknown1 = int.from_bytes(self._reader.read(4), byteorder='little')
-        self._unknown2 = int.from_bytes(self._reader.read(4), byteorder='little')
+                r = ((secondByte >> 3) & 0x1f) << 3
+                g = (((firstByte >> 5) & 0x7) | ((secondByte & 0x07) << 3)) << 2
+                b = (firstByte & 0x1f) << 3
+
+#                r = pixel & 0xff
+#                g = (pixel >> 8) & 0xff
+#                b = (pixel >> 16) & 0xff
+
+                a = (firstByte & 0x3f) << 2
+                b = (((firstByte >> 6) & 0x03) | ((secondByte & 0x0F) << 4))
+                g = (secondByte & 0x3f) << 2
+                r = (((secondByte >> 6) & 0x03) | ((thirddByte & 0x0F) << 4))
+
+                color = resources.image.color.Color.fromArgb(a, r, g, b)
+                image.putpixel((x, y), color)
+        return image
+
+    def readHeader16(self):
+        logging.info("Reading image header(readHeader16)...")
+        self._width = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._height = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._unknown1 = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._bitsPerPixel = int.from_bytes(self._reader.read(2), byteorder='little')
         self._step = int(self._bitsPerPixel / 8)
         self._rowLengthInBytes = self._width * self._step
         self._transparency = False
         logging.info("Image header was read:")
         logging.info(f"Width: {self._width}, Height: {self._height}, RowLength: {self._rowLengthInBytes}")
-        logging.info(f"unknown1: {self._unknown1}, _unknown2: {self._unknown2}, _step: {self._step}")
+        logging.info(f"unknown1: {self._unknown1}, _step: {self._step}")
+        logging.info(f"BPP: {self._bitsPerPixel}, Transparency: {self._transparency}")
+
+    def readHeader16E(self):
+        logging.info("Reading image header(readHeader16E)...")
+        self._width = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._height = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._unknown1 = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._bitsPerPixel = int.from_bytes(self._reader.read(2), byteorder='little')
+        self._datasize = int.from_bytes(self._reader.read(4), byteorder='little')
+        self._step = int(self._bitsPerPixel / 8)
+        self._rowLengthInBytes = self._width * self._step
+        #self._rowLengthInBytes = self._unknown1
+        self._transparency = False
+        logging.info("Image header was read:")
+        logging.info(f"Width: {self._width}, Height: {self._height}, RowLength: {self._rowLengthInBytes}")
+        logging.info(f"unknown1: {self._unknown1}, datalength: {self._datasize}, _step: {self._step}")
+        logging.info(f"BPP: {self._bitsPerPixel}, Transparency: {self._transparency}")
+
+    def readHeader(self):
+        logging.info("Reading image header(non-bip)...")
+        b = self._reader.read(4)
+        logging.debug("basename 7 byte hash " + ''.join(format(x, '02x') for x in b))
+        self._width = int.from_bytes(b, byteorder='little')
+        self._height = int.from_bytes(self._reader.read(4), byteorder='little')
+        self._bitsPerPixel = int.from_bytes(self._reader.read(4), byteorder='little')
+        self._unknown1 = int.from_bytes(self._reader.read(4), byteorder='little')
+        self._datasize = int.from_bytes(self._reader.read(4), byteorder='little')
+        self._step = int(self._bitsPerPixel / 8)
+        self._rowLengthInBytes = self._width * self._step
+        self._transparency = False
+        logging.info("Image header was read:")
+        logging.info(f"Width: {self._width}, Height: {self._height}, RowLength: {self._rowLengthInBytes}")
+        logging.info(f"unknown1: {self._unknown1}, _unknown2: {self._datasize}, _step: {self._step}")
         logging.info(f"BPP: {self._bitsPerPixel}, Transparency: {self._transparency}")
 
--- resources/image/writer.py
+++ resources/image/writer.py
@@ -1,18 +1,46 @@
 import logging
-
+from watchFaceParser.config import Config
 
 class Writer:
     signature = bytearray(b'BM\xff\xff')
-
+    signatureMini16BME = bytearray(b'BM\x65\x00')
+    signatureMini16 = bytearray(b'BM\x09\x00')
+    signatureMini24 = bytearray(b'BM\x1C\x00')
 
     def __init__(self, stream):
         self._writer = stream
 
-
     def write(self, image):
+
+        if Config.isGts2MiniMode():
+            self.writeGts2Mini(image)
+        else:
+            self.writeGts2(image)
+
+    def writeGts2Mini(self, image):
         self._image = image
         self._width = image.size[0]
         self._height = image.size[1]
+        self._bitsPerPixel = 16
+        import math
+        self._rowLengthInBytes = math.ceil(self._width * self._bitsPerPixel / 8)
+
+        if self.has_transparency(self._image):
+            rawData = self.getImageDataBme()
+            self._dataSize = len(rawData)
+            self._writer.write(Writer.signatureMini16BME)
+            self.writeHeaderMini16E()
+            self._writer.write(rawData)
+        else:
+            self._writer.write(Writer.signatureMini16)
+            self.writeHeaderMini16()
+            self.writeImageMini16()
+
+    def writeGts2(self, image):
+        self._image = image
+        self._width = image.size[0]
+        self._height = image.size[1]
+
         isBigImg = (self._width == 206 and self._height == 640) or (self._width == 152 and self._height == 472)
 
         self._bitsPerPixel = 32
@@ -33,6 +61,28 @@ class Writer:
         else:
             self.writeImage()
         #self.writeImage()
+
+    def writeHeaderMini16(self):
+        logging.debug("Writing image header mini...")
+        logging.debug(f"Width: {self._width}, Height: {self._height}, RowLength: {self._rowLengthInBytes}")
+        logging.debug(f"BPP: {self._bitsPerPixel}")
+
+        self._writer.write(self._width.to_bytes(2, byteorder='little'))
+        self._writer.write(self._height.to_bytes(2, byteorder='little'))
+        self._writer.write(self._rowLengthInBytes.to_bytes(2, byteorder='little'))
+        self._writer.write(self._bitsPerPixel.to_bytes(2, byteorder='little'))
+
+
+    def writeHeaderMini16E(self):
+        logging.debug("Writing image header mini...")
+        logging.debug(f"Width: {self._width}, Height: {self._height}, RowLength: {self._rowLengthInBytes}")
+        logging.debug(f"BPP: {self._bitsPerPixel}, DataSize: {self._dataSize}")
+
+        self._writer.write(self._width.to_bytes(2, byteorder='little'))
+        self._writer.write(self._height.to_bytes(2, byteorder='little'))
+        self._writer.write(self._rowLengthInBytes.to_bytes(2, byteorder='little'))
+        self._writer.write(self._bitsPerPixel.to_bytes(2, byteorder='little'))
+        self._writer.write(self._dataSize.to_bytes(4, byteorder='little'))
 
 
     def writeHeader(self):
@@ -70,14 +120,105 @@ class Writer:
             (r, g, b, a) = pixel
             #b = 0
             #g = 0
-            temp_b = ((b >> 3) & 0x1f);
-            temp_g = (((g >> 2) & 0x7) << 5);
-            firstByte = (temp_b | temp_g);
+            temp_b = ((b >> 3) & 0x1f)
+            temp_g = (((g >> 2) & 0x7) << 5)
+            firstByte = (temp_b | temp_g)
 
-            temp_g2 = ((g >> 5) & 0x07);
-            temp_r = (((r >> 3) & 0x1f) << 3);
-            secondByte = (temp_g2 | temp_r);
+            temp_g2 = ((g >> 5) & 0x07)
+            temp_r = (((r >> 3) & 0x1f) << 3)
+            secondByte = (temp_g2 | temp_r)
             self._writer.write(firstByte.to_bytes(1, byteorder='little'))
             self._writer.write(secondByte.to_bytes(1, byteorder='little'))
             #self._writer.write(b.to_bytes(1, byteorder='little'))
             #self._writer.write(g.to_bytes(1, byteorder='little'))
+
+    def writeImageMini16(self):
+        logging.debug("Writing image mini 16 bit/pixel...")
+
+        pixels = self._image.convert('RGBA')
+        data = pixels.getdata()
+
+        for pixel in data:
+            (r, g, b, a) = pixel
+
+            temp_b = ((b >> 3) & 0x1f)
+            temp_g = (((g >> 2) & 0x7) << 5)
+            firstByte = (temp_b | temp_g)
+
+            temp_g2 = ((g >> 5) & 0x07)
+            temp_r = (((r >> 3) & 0x1f) << 3)
+            secondByte = (temp_g2 | temp_r)
+            self._writer.write(firstByte.to_bytes(1, byteorder='little'))
+            self._writer.write(secondByte.to_bytes(1, byteorder='little'))
+
+    def getImageDataBme(self):
+        logging.debug("Generate image data bme 16 bit/pixel...")
+
+        pixels = self._image.convert('RGBA')
+        image_data = pixels.getdata()
+
+        result = bytearray()
+        y = 0
+        x = 0
+        temp_data = bytearray()
+        start_x = 0
+        pixel_width = 0
+
+        self._hasHalfAlpha = False
+
+        for y in range(self._height):
+            if len(temp_data) > 0:
+                #logging.debug(f"1 y: {y}, x: {start_x}, data_length: {len(temp_data)}")
+                result.extend(y.to_bytes(2, byteorder='little'))
+                result.extend(start_x.to_bytes(2, byteorder='little'))
+                result.extend(pixel_width.to_bytes(2, byteorder='little'))
+                result.extend(temp_data)
+            temp_data.clear()
+            start_x = 0
+            pixel_width = 0
+            for x in range(self._width):
+                pixel = image_data[y * self._width + x]
+
+                (r, g, b, a) = pixel
+                if r > 0 and g > 0 and b > 0:
+                    if len(temp_data) == 0:
+                        start_x = x
+                    temp_b = ((b >> 3) & 0x1f)
+                    temp_g = (((g >> 2) & 0x7) << 5)
+                    firstByte = (temp_b | temp_g)
+
+                    temp_g2 = ((g >> 5) & 0x07)
+                    temp_r = (((r >> 3) & 0x1f) << 3)
+                    secondByte = (temp_g2 | temp_r)
+
+                    temp_data.extend(firstByte.to_bytes(1, byteorder='little'))
+                    temp_data.extend(secondByte.to_bytes(1, byteorder='little'))
+                    pixel_width += 1
+                else:
+                    if len(temp_data) > 0:
+                        #logging.debug(f"2 y: {y}, x: {start_x}, data_length: {len(temp_data)}")
+                        result.extend(y.to_bytes(2, byteorder='little'))
+                        result.extend(start_x.to_bytes(2, byteorder='little'))
+                        result.extend(pixel_width.to_bytes(2, byteorder='little'))
+                        result.extend(temp_data)
+                    temp_data.clear()
+                    start_x = 0
+                    pixel_width = 0
+                if 0 < a < 255:
+                    self._hasHalfAlpha = True
+
+        logging.debug(f" hasHalfAlpha: {self._hasHalfAlpha}, has_transparency: {self.has_transparency(self._image)}")
+        return result
+
+    def has_transparency(self, img):
+        if img.mode == "P":
+            transparent = img.info.get("transparency", -1)
+            for _, index in img.getcolors():
+                if index == transparent:
+                    return True
+        elif img.mode == "RGBA":
+            extrema = img.getextrema()
+            if extrema[3][0] < 255:
+                return True
+
+        return False
