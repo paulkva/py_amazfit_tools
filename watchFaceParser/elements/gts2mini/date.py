9dad934eee2ada17618cbf1967347704796ead8e
Julien Maille <julien.maille@gmail.com>
2021-09-27 17:13:22 +0200

    NEW: OneLineMonthAndDay

--- watchFaceParser/elements/gts2mini/date.py
+++ watchFaceParser/elements/gts2mini/date.py
@@ -1,9 +1,10 @@
 from watchFaceParser.elements.gts2mini.dateElements.monthAndDay import MonthAndDay
+from watchFaceParser.elements.gts2mini.dateElements.oneLineMonthAndDay import OneLineMonthAndDay
 
 class Date:
     definitions = {
-        2: { 'Name': 'Unknown2', 'Type': 'long'},
-        4: { 'Name': 'Unknown4', 'Type': 'long'},
+        2: { 'Name': 'OneLineMonthAndDay', 'Type': OneLineMonthAndDay},
+        4: { 'Name': 'PaddingZeroMonth', 'Type': 'bool'},
         5: { 'Name': 'PaddingZeroDay', 'Type': 'bool'},
         6: { 'Name': 'Unknown6', 'Type': 'long'},
         9: { 'Name': 'MonthAndDay', 'Type': MonthAndDay},
