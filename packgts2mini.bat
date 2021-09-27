29cc98f4ae034d5796a5c09f195367a1813536cb
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-27 12:28:19 +0200

    pack script

---
+++ packgts2mini.bat
@@ -0,0 +1,20 @@
+@echo off
+
+IF "%1"=="" GOTO :Continue
+
+set file=%~n1
+set mPath=%~dp1
+set mPathScript=%~dp0
+echo %file%
+
+python %mPathScript%\main.py --gtr2 47 --file %1
+ren "%mPath%%file%_packed.bin.cmp" "%file%_packed_cmp.bin"
+
+echo.
+echo Install "%mPath%%file%_packed_cmp.bin" to Watch
+echo.
+pause
+
+:Continue
+IF "%1"=="" echo No json given. Usage: %~nx0 path\to\file.json
+
