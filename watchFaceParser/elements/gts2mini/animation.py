9a18eeba51490b75ada3b60d45738815e23f86eb
Julien Maille <julien.maille@gmail.com>
2021-09-27 13:51:57 +0200

    FIX: Animation array

--- watchFaceParser/elements/gts2mini/animation.py
+++ watchFaceParser/elements/gts2mini/animation.py
@@ -1,8 +1,7 @@
 from watchFaceParser.elements.gts2mini.animationElements.imageSetAnimation import ImageSetAnimation
-from watchFaceParser.elements.gts2mini.basicElements.iconSet import IconSet
 
 class Animation:
     definitions = {
-        1: {'Name': 'ImageSetAnimation', 'Type': ImageSetAnimation},
+        1: {'Name': 'ImageSetAnimation', 'Type': [ImageSetAnimation]},
     }
 
