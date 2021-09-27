f6f6ca790ad19839b8f0dc55fede4ecd00c0ed1e
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-27 16:27:48 +0200

    PaddingZeroMonth

--- watchFaceParser/elements/gts2mini/date.py
+++ watchFaceParser/elements/gts2mini/date.py
@@ -2,7 +2,7 @@ from watchFaceParser.elements.gts2mini.dateElements.monthAndDay import MonthAndD
 
 class Date:
     definitions = {
-        4: { 'Name': 'Unknown4', 'Type': 'long'},
+        4: { 'Name': 'PaddingZeroMonth', 'Type': 'long'},
         5: { 'Name': 'PaddingZeroDay', 'Type': 'bool'},
         6: { 'Name': 'Unknown6', 'Type': 'long'},
         9: { 'Name': 'MonthAndDay', 'Type': MonthAndDay},
