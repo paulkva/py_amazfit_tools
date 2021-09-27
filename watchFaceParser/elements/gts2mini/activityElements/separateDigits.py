409153e7b10f6b92e92e2d0a64d3ac79056b8b68
Julien Maille <julien.maille@gmail.com>
2021-09-27 16:54:05 +0200

    added comment

--- watchFaceParser/elements/gts2mini/activityElements/separateDigits.py
+++ watchFaceParser/elements/gts2mini/activityElements/separateDigits.py
@@ -2,7 +2,7 @@
 
 class ThreeDigits:
     definitions = {
-        1: { 'Name': 'Unknown1', 'Type': 'long?'},
+        1: { 'Name': 'Unknown1', 'Type': 'long?'}, #draw order?
         2: { 'Name': 'Hundreds', 'Type': ImageSet},
         3: { 'Name': 'Tens', 'Type': ImageSet},
         4: { 'Name': 'Ones', 'Type': ImageSet},
@@ -10,7 +10,7 @@ class ThreeDigits:
 
 class FourDigits:
     definitions = {
-        1: { 'Name': 'Unknown1', 'Type': 'long?'},
+        1: { 'Name': 'Unknown1', 'Type': 'long?'}, #draw order?
         2: { 'Name': 'Thousands', 'Type': ImageSet},
         3: { 'Name': 'Hundreds', 'Type': ImageSet},
         4: { 'Name': 'Tens', 'Type': ImageSet},
@@ -20,7 +20,7 @@ class FourDigits:
 
 class FiveDigits:
     definitions = {
-        1: { 'Name': 'Unknown1', 'Type': 'long?'},
+        1: { 'Name': 'Unknown1', 'Type': 'long?'}, #draw order?
         2: { 'Name': 'TenThousands', 'Type': ImageSet},
         3: { 'Name': 'Thousands', 'Type': ImageSet},
         4: { 'Name': 'Hundreds', 'Type': ImageSet},
