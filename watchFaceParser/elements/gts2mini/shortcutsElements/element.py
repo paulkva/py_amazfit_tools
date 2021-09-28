3613f1333d67f9265d5b08c62a6be5f8712ae871
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-28 13:34:14 +0200

    fix shortcuts

--- watchFaceParser/elements/gts2mini/shortcutsElements/element.py
+++ watchFaceParser/elements/gts2mini/shortcutsElements/element.py
@@ -3,7 +3,7 @@ class Element:
     definitions = {
         1: { 'Name': 'TopLeftX', 'Type': 'long'},
         2: { 'Name': 'TopLeftY', 'Type': 'long'},
-        3: { 'Name': 'Width', 'Type': 'long'},
-        4: { 'Name': 'Height', 'Type': 'long'},
+        3: { 'Name': 'BottomRightX', 'Type': 'long'},
+        4: { 'Name': 'BottomRightY', 'Type': 'long'},
         5: { 'Name': 'UnknownBoolean5', 'Type': 'bool'},
     }
--- watchFaceParser/models/gts2mini/shortcutType.py
+++ watchFaceParser/models/gts2mini/shortcutType.py
@@ -1,10 +1,58 @@
 class ShortcutType:
+    Unknown0 = 0
+    Unknown1 = 1
+    Unknown2 = 2
+    Unknown3 = 3
+    Unknown4 = 4
     Calories = 5
-    Music = 16
-   
+    Unknown6 = 6
+    Unknown7 = 7
+    Unknown8 = 8
+    Unknown9 = 9
+    Unknown10 = 10
+    Unknown11 = 11
+    Unknown12 = 12
+    Unknown13 = 13
+    Unknown14 = 14
+    Unknown15 = 15
+    Weather = 16
+    Unknown17 = 17
+    Unknown18 = 18
+    Unknown19 = 19
+    Unknown20 = 20
+    Unknown21 = 21
+    Unknown22 = 22
+    Unknown23 = 23
+    Unknown24 = 24
+    Pulse = 25
+
     Converter = {
-        Calories : "Calories", 
-        Music  : "Music",
+        Unknown0: "Unknown0",
+        Unknown1: "Unknown1",
+        Unknown2: "Unknown2",
+        Unknown3: "Unknown3",
+        Unknown4: "Unknown4",
+        Calories : "Calories",
+        Unknown6: "Unknown6",
+        Unknown7: "Unknown7",
+        Unknown8: "Unknown8",
+        Unknown9: "Unknown9",
+        Unknown10: "Unknown10",
+        Unknown11: "Unknown11",
+        Unknown12: "Unknown12",
+        Unknown13: "Unknown13",
+        Unknown14: "Unknown14",
+        Unknown15: "Unknown15",
+        Weather  : "Weather",
+        Unknown17: "Unknown17",
+        Unknown18: "Unknown18",
+        Unknown19: "Unknown19",
+        Unknown20: "Unknown20",
+        Unknown21: "Unknown21",
+        Unknown22: "Unknown22",
+        Unknown23: "Unknown23",
+        Unknown24: "Unknown24",
+        Pulse: "Pulse",
     }
 
     def __init__(self, flag):
