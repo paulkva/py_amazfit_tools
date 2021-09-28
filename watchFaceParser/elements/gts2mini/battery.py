4ccc126f4188d15c80bbd2c66ab6fbb0b6e9854e
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-28 09:02:36 +0200

    BatteryIcon -> ImageProgress

--- watchFaceParser/elements/gts2mini/battery.py
+++ watchFaceParser/elements/gts2mini/battery.py
@@ -6,7 +6,7 @@ from watchFaceParser.elements.gts2mini.basicElements.scale import Scale
 class Battery:
     definitions = {
         1: { 'Name': 'BatteryText', 'Type': Text},
-        2: { 'Name': 'BatteryIcon', 'Type': ImageSet},
+        2: { 'Name': 'ImageProgress', 'Type': ImageSet},
         3: { 'Name': 'IconSetProgress', 'Type': IconSet},
         4: {'Name': 'Unknown4', 'Type': 'long?'},
         5: {'Name': 'Scale', 'Type': Scale},
