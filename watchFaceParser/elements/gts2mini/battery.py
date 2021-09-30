197259339bc3323c84590549915a7ce00ca5a54e
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-30 10:10:23 +0200

    battery icon

--- watchFaceParser/elements/gts2mini/battery.py
+++ watchFaceParser/elements/gts2mini/battery.py
@@ -2,6 +2,7 @@ from watchFaceParser.elements.gts2mini.basicElements.text import Text
 from watchFaceParser.elements.gts2mini.basicElements.imageSet import ImageSet
 from watchFaceParser.elements.gts2mini.basicElements.iconSet import IconSet
 from watchFaceParser.elements.gts2mini.basicElements.scale import Scale
+from watchFaceParser.elements.gts2mini.basicElements.image import Image
 
 class Battery:
     definitions = {
@@ -10,4 +11,5 @@ class Battery:
         3: { 'Name': 'IconSetProgress', 'Type': IconSet},
         4: {'Name': 'Unknown4', 'Type': 'long?'},
         5: {'Name': 'Scale', 'Type': Scale},
+        6: {'Name': 'Icon', 'Type': Image},
     }
