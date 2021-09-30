13582b6b0c078844a6e4b143f01ca1f6c0027d0d
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-09-30 10:11:34 +0200

    add suffiximageindex

--- watchFaceParser/elements/gts2mini/weatherElements/humidity.py
+++ watchFaceParser/elements/gts2mini/weatherElements/humidity.py
@@ -5,6 +5,7 @@ from watchFaceParser.elements.gts2mini.basicElements.image import Image
 class Humidity:
     definitions = {
         1: {'Name': 'HumidityNumber', 'Type': Number},
+        2: {'Name': 'SuffixImageIndex', 'Type': 'long'},
         9: {'Name': 'HumidityIcon', 'Type': Image},
     }
 
