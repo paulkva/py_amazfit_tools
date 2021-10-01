f7db216a6f50ed614892ce1760efbe7191443101
Julien Maille <julien.maille@gmail.com>
2021-10-01 20:47:17 +0200

    NEW: support for CircleScale

--- watchFaceParser/elements/gts2mini/progress.py
+++ watchFaceParser/elements/gts2mini/progress.py
@@ -1,13 +1,14 @@
 from watchFaceParser.elements.gts2mini.basicElements.imageSet import ImageSet
 from watchFaceParser.elements.gts2mini.basicElements.iconSet import IconSet
 from watchFaceParser.elements.gts2mini.basicElements.scale import Scale
+from watchFaceParser.elements.gts2mini.basicElements.circleScale import CircleScale
 
 class Progress:
     definitions = {
         1: {'Name': 'Unknown1', 'Type': 'long?'},
-        2: { 'Name': 'ImageProgress', 'Type': ImageSet},
-        3: { 'Name': 'IconSetProgress', 'Type': IconSet},
-        4: {'Name': 'Unknown4', 'Type': 'long?'},
+        2: {'Name': 'ImageProgress', 'Type': ImageSet},
+        3: {'Name': 'IconSetProgress', 'Type': IconSet},
+        4: {'Name': 'CircleScale', 'Type': CircleScale},
         5: {'Name': 'Unknown5', 'Type': 'long?'},
         6: {'Name': 'Scale', 'Type': Scale},
     }
