18556c70f7a4ac51197bb7fda52e5fb8c0c1290e
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-10-01 10:26:35 +0200

    shortcut

--- watchFaceParser/elements/gts2mini/weatherElements/uvindexElement.py
+++ watchFaceParser/elements/gts2mini/weatherElements/uvindexElement.py
@@ -1,11 +1,12 @@
 from watchFaceParser.elements.gts2mini.basicElements.number import Number
 from watchFaceParser.elements.gts2mini.basicElements.image import Image
-
+from watchFaceParser.elements.gts2mini.shortcutElement import ShortcutElement
 
 class UVindex:
     definitions = {
         1: {'Name': 'UVindexNumber', 'Type': Number},
         2: {'Name': 'SuffixImageIndex', 'Type': 'long'},
+        3: {'Name': 'Shortcut', 'Type': ShortcutElement},
         9: {'Name': 'UVindexIcon', 'Type': Image},
     }
 
