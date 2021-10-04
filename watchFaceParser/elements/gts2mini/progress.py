38145eeb5ffe89a6b47c1d8780f1bd74044813b2
nikolas.maletschkin <nikolas.maletschkin@himolla.com>
2021-10-04 13:33:45 +0200

    fix progress element

--- watchFaceParser/elements/gts2mini/progress.py
+++ watchFaceParser/elements/gts2mini/progress.py
@@ -12,24 +12,24 @@ class Progress:
         4: {'Name': 'CircleScale', 'Type': CircleScale},
         5: {'Name': 'Unknown5', 'Type': 'long?'},
         6: {'Name': 'Scale', 'Type': Scale},
-        7: {'Name': 'NoDataImageIndex', 'Type': 'long?'},
-        8: {'Name': 'UnknownImageIndex', 'Type': 'long?'},
+        7: {'Name': 'NoDataImage', 'Type': Image},
+        8: {'Name': 'UnknownImage', 'Type': Image},
     }
 
 class ProgressAlt1:
     definitions = {
         2: {'Name': 'ImageProgress', 'Type': ImageSet},
-        6: {'Name': 'NoDataImageIndex', 'Type': Image},
+        6: {'Name': 'NoDataImage', 'Type': Image},
     }
 
 class ProgressAlt2:
     definitions = {
         1: {'Name': 'ImageProgress', 'Type': ImageSet},
-        5: {'Name': 'NoDataImageIndex', 'Type': Image},
+        5: {'Name': 'NoDataImage', 'Type': Image},
     }
 
 class ProgressAlt3:
     definitions = {
         2: {'Name': 'ImageProgress', 'Type': ImageSet},
-        8: {'Name': 'NoDataImageIndex', 'Type': Image},
+        8: {'Name': 'NoDataImage', 'Type': Image},
     }
